ce415cd62adf9cbaac222e84fb17dc8b3c190f68
alissonfluck <alissonfluck@proton.me>
2026-09-25 13:03:16 -0300

    feat: filtra extracao pelas 4 maiores montadoras do mercado nacional

--- extracao_modelos.py
+++ extracao_modelos.py
@@ -4,38 +4,41 @@ import time
 
 print("Lendo o Data Lake local...")
 
-with open("marcas_fipe.json", "r") as file:
+with open("data/marcas_fipe.json", "r") as file:
     lista_marcas = json.load(file)
 
 todos_modelos = []
 
+marcas_alvo = ["21", "22", "23", "59"]
 
-print("Iniciando a extração da segunda camada (Modelos)...")
-for marca in lista_marcas[:3]:
+
+print("Iniciando a extração em escala (Modelos Fiat, Ford, GM e VolksWagen)...")
+for marca in lista_marcas:
     codigo_marca = marca["valor"]
-    nome_marca = marca["nome"]
 
-    print(f"Buscando modelos da marca: {nome_marca}")
+    if codigo_marca in marcas_alvo:
+        nome_marca = marca["nome"]
+        print(f"Buscando portfólio completo da marca: {nome_marca}")
 
-    url_modelos = f"https://parallelum.com.br/fipe/api/v1/carros/marcas/{codigo_marca}/modelos"
-    response = requests.get(url_modelos)
+        url_modelos = f"https://parallelum.com.br/fipe/api/v1/carros/marcas/{codigo_marca}/modelos"
+        response = requests.get(url_modelos)
 
-    if response.status_code == 200:
-        dados_modelo = response.json()
+        if response.status_code == 200:
+            dados_modelo = response.json()
 
-        todos_modelos.append({
-            "codigo_marca": codigo_marca,
-            "marca": nome_marca,
-            "modelos": dados_modelo["modelos"]
-        })
-    else:
-        print(f"Erro na marca {nome_marca}. Status: {response.status_code}")
+            todos_modelos.append({
+                "codigo_marca": codigo_marca,
+                "marca": nome_marca,
+                "modelos": dados_modelo["modelos"]
+            })
+        else:
+            print(f"Erro na marca {nome_marca}. Status: {response.status_code}")
 
-    time.sleep(1)
+        time.sleep(1)
 
 
 print("\nSalvando os dados consolidados...")
 with open("data/modelos_fipe.json", "w") as arquivo_final:
     json.dump(todos_modelos, arquivo_final, indent=4)
 
-print("Sucesso! Arquivo modelos_fipe.json gerado localmente.")
+print("Sucesso! Extração concluída.")
